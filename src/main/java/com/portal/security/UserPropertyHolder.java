d2de2d7271c734d519f442fbd4114421e0dcf690
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-04-19 17:05:29 -0300

    docs for user property holder

--- src/main/java/com/portal/security/UserPropertyHolder.java
+++ src/main/java/com/portal/security/UserPropertyHolder.java
@@ -11,15 +11,50 @@ public class UserPropertyHolder {
 	private String name;
 	private final Map<String, ServiceApi> authenticatedServices = new ConcurrentHashMap<>();
 
+	/**
+	 * Register this service to the hash table.
+	 * 
+	 * @param key
+	 * @param service
+	 */
 	public void registerAuthenticatedService(String key, ServiceApi service) {
 		this.authenticatedServices.putIfAbsent(key, service);
 	}
 
+	/**
+	 * remove this service from hash table.
+	 * 
+	 * @param key
+	 */
 	public void unRegisterAuthenticatedService(String key) {
 		this.authenticatedServices.remove(key);
 	}
 
+	/**
+	 * Get the service by the key.
+	 * 
+	 * @param key
+	 * @return
+	 */
 	public ServiceApi findServiceApi(String key) {
 		return this.authenticatedServices.get(key);
+	}
+
+	/**
+	 * Set the global name for the session user.
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Get the name of global user.
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return name;
 	}
 }
