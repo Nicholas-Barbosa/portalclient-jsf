7fdd13ce046d00905c288c5c0d858b927f856ee1
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-04-19 17:03:01 -0300

    UserPropertyHolder

--- src/main/java/com/portal/security/UserPropertyHolder.java
+++ src/main/java/com/portal/security/UserPropertyHolder.java
@@ -10,6 +10,16 @@ public class UserPropertyHolder {
 
 	private String name;
 	private final Map<String, ServiceApi> authenticatedServices = new ConcurrentHashMap<>();
-	
-	
+
+	public void registerAuthenticatedService(String key, ServiceApi service) {
+		this.authenticatedServices.putIfAbsent(key, service);
+	}
+
+	public void unRegisterAuthenticatedService(String key) {
+		this.authenticatedServices.remove(key);
+	}
+
+	public ServiceApi findServiceApi(String key) {
+		return this.authenticatedServices.get(key);
+	}
 }
