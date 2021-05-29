d16829997d75a59b9ae719eeb370e1b78890ab60
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-05-28 23:09:12 -0300

    testing deflater

--- src/test/java/com/portal/google/storage/BucketClient.java
+++ src/test/java/com/portal/google/storage/BucketClient.java
@@ -1,7 +1,12 @@
 package com.portal.google.storage;
 
-import java.nio.file.Path;
-import java.util.Base64;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
@@ -11,7 +16,6 @@ import com.google.cloud.storage.Blob.BlobSourceOption;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.cloud.storage.Storage.BlobGetOption;
 
 class BucketClient {
 
@@ -31,10 +35,30 @@ class BucketClient {
 		Bucket bucket = storage.get(bucketName);
 		Blob object = bucket.get("budget.pdf");
 		byte[] content = object.getContent();
+		System.out.println("downalod link " + object.getMediaLink());
 		System.out.println("length " + content.length);
 //		bucket.get("budget.pdf")
 //				.downloadTo(Path.of("C:\\Users\\nicho\\OneDrive\\Documentos\\gCloud storage\\budget.pdf"));
 		// System.out.printf("Bucket %s created.%n", bucket.getName());
 	}
 
+	@Test
+	void testCompactToGzip() {
+		Storage storage = StorageOptions.getDefaultInstance().getService();
+
+		// The name for the new bucket
+		String bucketName = "streams-portal"; // "my-new-bucket";
+
+		// Creates the new bucket
+		Bucket bucket = storage.get(bucketName);
+		Blob object = bucket.get("imagens_tratadas/GA1320.JPG");
+		byte[] imageObject = object.getContent();
+		System.out.println("Size bytes[] " + imageObject.length);
+
+		Deflater deflater = new Deflater();
+		byte[] deflatedStreams = new byte[1048576];
+		deflater.setInput(imageObject);
+		deflater.finish();
+		System.out.println("length deflated " + +deflater.deflate(deflatedStreams));
+	}
 }
