ec324e8400e63b2050171edf23ca086d96e99cae
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-07-22 19:15:58 -0300

    implemented pagination for budgets

--- src/main/java/com/portal/client/controller/BudgetListController.java
+++ src/main/java/com/portal/client/controller/BudgetListController.java
@@ -8,6 +8,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.data.PageEvent;
+
 import com.portal.client.service.BudgetService;
 import com.portal.client.ui.lazy.datamodel.BudgetLazyDataModel;
 import com.portal.client.ui.lazy.datamodel.LazyDataModelBase;
@@ -28,15 +30,19 @@ public class BudgetListController {
 		this.buService = buService;
 	}
 
-	public void loadBudgets() {
+	public void loadBudgets(int page) {
 		if (budgets == null)
 			this.budgets = new BudgetLazyDataModel();
 		try {
-			LazyPopulateUtils.populate(budgets, buService.findAll(1, 15));
+			LazyPopulateUtils.populate(budgets, buService.findAll(page, 15));
 		} catch (SocketTimeoutException | SocketException | TimeoutException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public void onPage(PageEvent page) {
+		this.loadBudgets(page.getPage() + 1);
 	}
 
 	public LazyDataModelBase<Budget> getBudgets() {
