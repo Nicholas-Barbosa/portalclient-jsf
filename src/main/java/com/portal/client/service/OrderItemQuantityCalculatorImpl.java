49c1f4e4db00d16db4db5265a92348466891b83b
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-09-30 22:18:46 -0300

    improving in calc method in OrderItemQuantityCalculatorImpl

--- src/main/java/com/portal/client/service/OrderItemQuantityCalculatorImpl.java
+++ src/main/java/com/portal/client/service/OrderItemQuantityCalculatorImpl.java
@@ -1,12 +1,14 @@
 package com.portal.client.service;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import com.portal.client.exception.ItemQuantityNotAllowed;
 import com.portal.client.vo.Item;
+import com.portal.client.vo.ItemValue;
 import com.portal.client.vo.Order;
 
 @ApplicationScoped
@@ -17,14 +19,22 @@ public class OrderItemQuantityCalculatorImpl implements OrderItemQuantityCalcula
 	 */
 	private static final long serialVersionUID = -5405223065684465910L;
 	@Inject
-	private OrderCommonBehaviorHelper orderBehavior;
-	@Inject
 	private ItemService itemService;
 
 	@Override
 	public void calc(Order order, Item item, int quantity) throws ItemQuantityNotAllowed {
+		ItemValue itemValue = item.getValue();
+
+		BigDecimal oldTotalValue = itemValue.getTotalValue();
+		BigDecimal oldTotalGrossValue = itemValue.getTotalGrossValue();
+		BigDecimal oldTotalStValue = itemValue.getTotalStValue();
+
 		itemService.calculateDueQuantity(item, quantity);
-		orderBehavior.calculateTotals(order);
+
+		order.setGrossValue(order.getGrossValue().subtract(oldTotalGrossValue).add(itemValue.getTotalGrossValue()));
+		order.setLiquidValue(order.getLiquidValue().subtract(oldTotalValue).add(itemValue.getTotalValue()));
+		order.setStValue(order.getStValue().subtract(oldTotalStValue).add(itemValue.getTotalStValue()));
+
 	}
 
 }
