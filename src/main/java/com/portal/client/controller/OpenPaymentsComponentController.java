89ffb03461b671e735e61df98207156de33914a7
Nicholas Barbosa <68034898+Nicholas-Barbosa@users.noreply.github.com>
2021-11-03 15:15:48 -0300

    fixed openPayments composite component

--- src/main/java/com/portal/client/controller/OpenPaymentsComponentController.java
+++ src/main/java/com/portal/client/controller/OpenPaymentsComponentController.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.portal.client.service.crud.OpenPaymentService;
+import com.portal.client.util.jsf.FacesUtils;
 
 @RequestScoped
 @Named
@@ -18,9 +19,12 @@ public class OpenPaymentsComponentController {
 	public void test() {
 		System.out.println("Test");
 	}
-	public void findPayments(int page, String code, String store) {
-		service.findByCustomerCodeStore(page, pageSize, code, store).ifPresent(paymentPage -> {
 
+	public void findPayments(int page, String code, String store) {
+		service.findByCustomerCodeStore(page, pageSize, code, store).ifPresentOrElse(paymentPage -> {
+
+		}, () -> {
+			FacesUtils.error(null, "Não há títulos para este cliente", null, "growl");
 		});
 	}
 }
